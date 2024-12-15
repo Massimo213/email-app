778714517317038187bac3361245c29595096824
Massimo213 <yahyastartwell@gmail.com>
2024-12-15 11:46:00 -0500

    update

--- next.config.js
+++ next.config.js
@@ -1,17 +1,19 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-import "./src/env.js";
-
-/** @type {import("next").NextConfig} */
-const config = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
   typescript: {
-    ignoreBuildErrors:true,
+    ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
+  reactStrictMode: false,
 };
 
-export default config;
+export default nextConfig;
